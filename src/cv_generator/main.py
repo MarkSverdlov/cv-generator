182c8c12cf81bb684add153fc3f0737a0b9ce72c
Mark Sverdlov <marksve039@gmail.com>
2026-08-19 10:39:19 +0300

    Reduce the argument list of the script

--- src/cv_generator/main.py
+++ src/cv_generator/main.py
@@ -1,5 +1,6 @@
 import os
 import pathlib
+import sys
 
 import typer
 
@@ -20,29 +21,39 @@ def detect_language(text: str) -> str:
     return "he" if hebrew / total > 0.3 else "en"
 
 
-def main(cv_path: pathlib.Path, role_path: pathlib.Path, role_name: str) -> None:
-    with open(cv_path) as file:
-        cv_json = file.read()
+def main(role_path: pathlib.Path, role_name: str | None = None) -> None:
+    if role_name is None:
+        role_name = role_path.stem
+    role_cv_path = pathlib.Path(f"~/curriculum-vitae/{role_name}").expanduser()
+    try:
+        os.mkdir(role_cv_path)
+    except FileExistsError:
+        print(f"{role_name} already exists. Please choose another name")
+        sys.exit(0)
     with open(role_path, encoding="utf-8") as file:
         role_ad = file.read()
+    language = detect_language(role_ad)
+    if language == "he":
+        from cv_gen_he import generate_cv
 
-    os.mkdir(role_name)
+        cv_path = pathlib.Path("~/curriculum-vitae/cv-he.json").expanduser()
+    else:
+        from cv_gen import generate_cv
+
+        cv_path = pathlib.Path("~/curriculum-vitae/cv.json").expanduser()
+    with open(cv_path) as file:
+        cv_json = file.read()
 
     role_fit = suggest_fit(cv_json, role_ad)
     modification = suggest_modification(cv_json, role_fit)
     cv = CV.model_validate_json(cv_json)
     new_cv = modify(cv, modification)
 
-    with open(f"{role_name}/role-fit-summary.txt", "w") as file:
+    with open(role_cv_path / "role-fit-summary.txt", "w") as file:
         file.write(role_fit)
 
-    language = detect_language(role_ad)
-    if language == "he":
-        from cv_gen_he import generate_cv
-    else:
-        from cv_gen import generate_cv
     new_cv_txt = generate_cv(new_cv)
-    with open(f"{role_name}/mark-sverdlov-cv.tex", "w") as file:
+    with open(role_cv_path / "mark-sverdlov-cv.tex", "w") as file:
         file.write(new_cv_txt)
 
 
